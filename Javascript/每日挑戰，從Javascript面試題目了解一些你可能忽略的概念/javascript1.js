2f1f9502b56bf095c81ff0e8b1a1e58d79aba393
Hung che wei <fish25298522@gmail.com>
2024-08-06 02:21:23 +0800

    upload

---
+++ Javascript/每日挑戰，從Javascript面試題目了解一些你可能忽略的概念/javascript1.js
@@ -0,0 +1,5 @@
+//請解釋var const & let的差別
+var author = "Danny";
+var author = "Wang";
+
+console.log(author);
--- Javascript/每日挑戰，從Javascript面試題目了解一些你可能忽略的概念/javascript6.js
+++ Javascript/每日挑戰，從Javascript面試題目了解一些你可能忽略的概念/javascript6.js
@@ -1,15 +1,13 @@
 //我們知道JS中有兩種宣告函數的方法，請問以下兩種方法有差別嗎?
 
- 
-console.log(add(3,5))
-var add = function(a, b) {
-  return a + b
-}
-
+console.log(add(3, 5));
+var add = function (a, b) {
+  return a + b;
+};
 
 /*
 其實原因相當的單純，我們說過提升是將所有的宣告(declaration)放到記憶體位置的行為，
-且只有宣告會被提升、復職的行為並不會一併被提升，
+且只有宣告會被提升、賦值的行為並不會一併被提升，
 所以情境一的程式碼你可以這樣理解。
 
 var add
@@ -17,6 +15,4 @@ console.log(add(3,5))  add變數被提升，但它仍還沒被賦值為一個函
 add = function(a, b) {
   return a + b
 }
-*/ 
-
-
+*/
