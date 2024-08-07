e8ec9ab8c94e5a7cb0a3ba34fbcf9ddb649a6a6d
Hung che wei <35070932+111504@users.noreply.github.com>
2024-08-07 08:52:10 +0800

    upload

--- Javascript/每日挑戰，從Javascript面試題目了解一些你可能忽略的概念/javascript30.js
+++ Javascript/每日挑戰，從Javascript面試題目了解一些你可能忽略的概念/javascript30.js
@@ -1,8 +1,9 @@
 //請寫一個簡單的函數檢測輸入的字串是否為回文(palindrome)
-function palindrome(str){
-   str=str.toLowerCase();
-   for (let i = 0; i < Math.floor(filteredStr.length / 2); i++) {
-    if (filteredStr[i] !== filteredStr[filteredStr.length - i -1]) {
+function psilnfrome(str){
+  str=str.toLowerCase();//讓字串變為小寫
+  //頭尾比對
+  for(let i=0;i<Math.floor(str.length/2);i++){
+    if(str[i]!==str[str.length-i-1]){
       return false
     }
   }
